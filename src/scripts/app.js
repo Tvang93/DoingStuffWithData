beb66bb9d324c2afb31029f4271685a1e3a29444
Tvang93 <vangt39@gmail.com>
2025-02-13 16:08:33 -0800

    made a sort list function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,6 +9,11 @@ const displayStartAmount = document.getElementById("displayStartAmount");
 const displayEndAmount = document.getElementById("displayEndAmount");
 const totalEntries = document.getElementById("totalEntries");
 
+const sortById = document.getElementById("sortById");
+const sortByFirstName = document.getElementById("sortByFirstName");
+const sortByLastName = document.getElementById("sortByLastName");
+const sortByHeight = document.getElementById("sortByHeight");
+const sortByAge = document.getElementById("sortByAge");
 const listContainer = document.getElementById("listContainer");
 
 const prevBtn = document.getElementById("prevBtn");
@@ -17,13 +22,16 @@ const nextBtn = document.getElementById("nextBtn");
 let peopleList;
 let counter1 = 1
 let counter2 = 10;
-let sortBy = {
-    id: true,
-    firstName: false,
-    LastName: false,
-    height: false,
-    age: false,
-}
+// let sortBy = {
+//     id: true,
+//     firstName: false,
+//     LastName: false,
+//     height: false,
+//     age: false,
+// }
+
+let sortCategory = "id"
+
 let multiplier = 1
 let newAddCounter = counter2*multiplier
 
@@ -38,13 +46,7 @@ const setListbyId = async(num1, num2) => {
     peopleList = await fetchData();
     console.log(peopleList.peopleList);
     let shallowCopy = [...peopleList.peopleList];
-    shallowCopy.slice(num1-1, num2).forEach(entry => {
-        listContainer.appendChild(createH1(entry.id))
-        listContainer.appendChild(createH1(entry.firstName))
-        listContainer.appendChild(createH1(entry.lastName))
-        listContainer.appendChild(createH1(entry.height))
-        listContainer.appendChild(createH1(entry.age))
-    });
+    SortList(shallowCopy, num1, num2)
     totalEntries.innerText = peopleList.peopleList.length;
     displayStartAmount.innerText = counter1;
     if(counter2 > peopleList.peopleList.length){
@@ -107,6 +109,54 @@ prevBtn.addEventListener('click', () => {
         setListbyId(counter1, counter2);
     }
 })
+
+sortById.addEventListener('click', () => {
+    sortCategory = "id";
+    setListbyId(counter1, counter2);
+})
+
+sortByFirstName.addEventListener('click', () => {
+    sortCategory = "firstName";
+    setListbyId(counter1, counter2);
+})
+
+sortByLastName.addEventListener('click', () => {
+    sortCategory = "lastName";
+    setListbyId(counter1, counter2);
+})
+
+sortByHeight.addEventListener('click', () => {
+    sortCategory = "height";
+    setListbyId(counter1, counter2);
+})
+
+sortByAge.addEventListener('click', () => {
+    sortCategory = "age";
+    setListbyId(counter1, counter2);
+})
+
+
+const SortList = (list, num1, num2) => {
+    switch(sortCategory){
+        case "id":
+            list.slice(num1-1, num2).forEach(entry => {
+                listContainer.appendChild(createH1(entry.id))
+                listContainer.appendChild(createH1(entry.firstName))
+                listContainer.appendChild(createH1(entry.lastName))
+                listContainer.appendChild(createH1(entry.height))
+                listContainer.appendChild(createH1(entry.age))
+            });
+            break;
+        case "firstName":
+            break;
+        case "lastName":
+            break;
+        case "height":
+            break;
+        default:
+            break;
+    }
+}
 
 console.log("reloaded")
 setListbyId(counter1, counter2*multiplier);
