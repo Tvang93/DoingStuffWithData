2c41c4424cfafdd608d3b74da1b1dd375d59cfbd
Tvang93 <vangt39@gmail.com>
2025-02-13 16:36:17 -0800

    sort by various categories

--- src/scripts/sortList.js
+++ src/scripts/sortList.js
@@ -10,13 +10,48 @@ const SortList = (category, list, num1, num2) => {
             });
             break;
         case "firstName":
-            
+            list.sort((a, b) => a.firstName.localeCompare(b.firstName))
+            .slice(num1-1, num2)
+            .forEach(entry => {
+                listContainer.appendChild(createH1(entry.id))
+                listContainer.appendChild(createH1(entry.firstName))
+                listContainer.appendChild(createH1(entry.lastName))
+                listContainer.appendChild(createH1(entry.height))
+                listContainer.appendChild(createH1(entry.age))
+            });
             break;
         case "lastName":
+            list.sort((a, b) => a.firstName.localeCompare(b.firstName))
+            .slice(num1-1, num2)
+            .forEach(entry => {
+                listContainer.appendChild(createH1(entry.id))
+                listContainer.appendChild(createH1(entry.firstName))
+                listContainer.appendChild(createH1(entry.lastName))
+                listContainer.appendChild(createH1(entry.height))
+                listContainer.appendChild(createH1(entry.age))
+            });
             break;
         case "height":
+            list.sort((i, j) => i.height - j.height)
+            .slice(num1-1, num2)
+            .forEach(entry => {
+                listContainer.appendChild(createH1(entry.id))
+                listContainer.appendChild(createH1(entry.firstName))
+                listContainer.appendChild(createH1(entry.lastName))
+                listContainer.appendChild(createH1(entry.height))
+                listContainer.appendChild(createH1(entry.age))
+            });
             break;
         default:
+            list.sort((i, j) => i.age - j.age)
+            .slice(num1-1, num2)
+            .forEach(entry => {
+                listContainer.appendChild(createH1(entry.id))
+                listContainer.appendChild(createH1(entry.firstName))
+                listContainer.appendChild(createH1(entry.lastName))
+                listContainer.appendChild(createH1(entry.height))
+                listContainer.appendChild(createH1(entry.age))
+            });
             break;
     }
 }
